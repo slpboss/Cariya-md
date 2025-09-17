62e0e268645155918f3bd108fc627c99cf24f486
slpboss <chamalsha166@gmail.com>
2025-09-17 23:41:14 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,5 +8,5 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || ''
+    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD~AAIgDACZ#xKbF40EKYIAw9xfJwwTrlLcqCTyfUnLpbhkIFmnRE1M'
 };
